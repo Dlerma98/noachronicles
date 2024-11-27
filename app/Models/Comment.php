d08fdd7ec195d21dd2d668c69cfa4aafdf676464
Dlerma98 <davidglerma1990@gmail.com>
2024-11-27 15:45:11 +0100

    modelos y relaciones

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['content', 'user_id', 'post_id', 'parent_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['title', 'summary', 'content', 'thumbnail', 'user_id'];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
